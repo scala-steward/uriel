3098c495d43063e05fe3e70520e215f314778efe
scala-steward <scala-steward@users.noreply.github.com>
2025-09-01 13:50:08 +0000

    deploy: bb8881587900b5bfd554dfc7a333b1464107812c

---
+++ shared/src/main/scala/Demo.scala
@@ -0,0 +1,65 @@
+//
+//import ai.dragonfly.uriel.*
+//import ai.dragonfly.uriel.cie.WorkingSpace
+//import ai.dragonfly.uriel.ColorContext.sRGB
+//import ai.dragonfly.math.Random.defaultRandom
+//
+//import scala.collection.mutable
+//import scala.util.Random
+//import palette.immutable.ColorPalette
+//
+//import scala.collection.immutable
+//import scalatags.Text.all.*
+//
+//object Demo {
+//
+//  val r:Random = defaultRandom
+//
+//  def main(args: Array[String]): Unit = {
+//    import sRGB.*
+//
+//    val commonSpaces: Seq[ColorSpace[?]] = Seq[ColorSpace[?]](RGB, ARGB32, ARGB64, RGBA32, RGBA64, CMYK, HSL, HSV, XYZ, Lab, Luv)
+//
+//    for (space <- commonSpaces) {
+//      // generate random color palette
+//      val colors: mutable.HashSet[C] = mutable.HashSet[C]()
+//      while (colors.size < 20) {
+//        val c: C = space.random().asInstanceOf[C]
+//        if (!colors.contains(c)) colors.add(c)
+//      }
+//
+//      val cp = ColorPalette[C](immutable.HashMap.from[C, Int](colors.map {_ -> r.nextInt(1000)}))
+//
+//      val content = div(
+//        div(
+//          s"Random Color Palette from $space space:",
+//          br,
+//          table(
+//            tr(td("Color"), td("Frequency")),
+//            cp.colorFrequencies.map {
+//              cf => tr(td(backgroundColor := ARGB32.fromRGB(cf.color.toRGB.asInstanceOf[sRGB.RGB]).html())(raw("&nbsp;")), td(f"${cf.frequency}%.3f%%"))
+//            }
+//          )
+//        ),
+//        div(
+//          "Generate random colors and search palette for nearest match:",
+//          br,
+//          table(
+//            tr(td("Random Color"), td("Nearest Match from Palette"), td(s"Distance in $space ColorSpace")),
+//            (1 to 10).map(_ => {
+//              val qc: C = space.random().asInstanceOf[C]
+//              val m: C = cp.nearestMatch(qc).color
+//
+//              tr(
+//                td(backgroundColor := ARGB32.fromRGB(qc.toRGB.asInstanceOf[sRGB.RGB]).html())(raw("&nbsp;")),
+//                td(backgroundColor := ARGB32.fromRGB(m.toRGB.asInstanceOf[sRGB.RGB]).html())(raw("&nbsp;")),
+//                td(f"${qc.similarity(m)}%.3f%%")
+//              )
+//            })
+//          )
+//        )
+//      )
+//      println(content)
+//    }
+//  }
+//}
